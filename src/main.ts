f9680a632f555fe6f686f0c378bec6926015ddea
lukaLomadze <lloma23@freeuni.edu.ge>
2026-02-16 18:17:37 +0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   
 
   app.enableCors({
-    origin: process.env.FRONT_URL ?? 'http://localhost:3000',
+    origin: [process.env.FRONT_URL ,'http://localhost:3000'],
   });
 
   const config = new DocumentBuilder()
